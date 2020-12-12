d9c9508e5c8d46cf2eae7cd1c00010900065b3e3
jhpiedrahitao <jhpiedrahitao@unal.edu.co>
2020-12-12 18:40:45 -0500

    se modifica el metodo registro get all para que entrege una lista

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ from fastapi import FastAPI, HTTPException
 
 api = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+origins = [
+"http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
+"http://localhost", "http://localhost:8080"
+]
+api.add_middleware(
+CORSMiddleware, allow_origins=origins,
+allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
+)
+
 @api.post("/cliente/registroSave")
 async def save_cliente(cliente: Cliente):
     cliente_in_db=ClienteInDB(**cliente.dict())
@@ -50,9 +60,9 @@ async def get_cliente(documento:str):
 @api.get("/cliente/registroGetAll")
 async def get_clientes():
     clientes_in_db=cliente_db.get_all_clientes()
-    clientes={}
+    clientes=[]
     for k,v in clientes_in_db.items():
-        clientes[k]=Cliente(**v.dict())
+        clientes.append(Cliente(**v.dict()))
     return clientes
 
 @api.get("/")
